08efdea59ef9a47557fcd02bcd0dd98929ba1732
nathan varin <nathan.varin@outlook.fr>
2021-05-09 11:58:58 +0200

    Début tableau

--- test_tableau.js
+++ test_tableau.js
@@ -1,14 +1,55 @@
+//Variables globales
+var TableauJeu ;
+var tresorCarte="Trésor" ;
+var nbMax=10 ;
+var tresorX ;
+var tresorY ;
+
+
+//Création du tableau (en mémoire)
+function Tab2D(x, y) {
+	var table = new Array(x);
+	for (var i = 0; i < table.length; i++) {
+		table[i] = new Array(y);
+	}
+	return table ;
+}
+
+//Fonction d'initialisation du jeu
+function init() {
+	TableauJeu=Tab2D(10, 10) ;
+
+	//Définir l'emplacement du trésor avec un Random
+	tresorX = Math.floor(Math.random() * nbMax + 1) ;
+	tresorY = Math.floor(Math.random() * nbMax + 1) ;
+
+	//Ajouter le trésor dans le tableau (en mémoire)
+	TableauJeu[tresorX][tresorY]=tresorCarte ;
+	console.log(TableauJeu[tresorX][tresorY]) ;
+	console.log(TableauJeu) ;
+
+	//Générer le tableau en html
+	var carteHTML=document.getElementById("carte") ;
+	for(var i=0; i<10;i++)
+	{
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // creation par Sebastien meunier et Nathan Varin
 // derniere modification = 26/04/2021
-console.log()
-// va creer un tableau en 2 dimensions dans la mémoire de Javascript
-function Tab2D(x, y) {
-	let table = new Array(x);
-	for (let h = 0; 1 < table.lenght; h++) {
-		table[h] = new Array(y);
-	}
-	return table
-}
 // va créer un tableau en 2 dimensions sur la page html
 function Tableau2D(ligne, colonne, emplacement) {
 	texte="<table>"; 
@@ -26,22 +67,10 @@ function Tableau2D(ligne, colonne, emplacement) {
 	document.getElementById(emplacement).innerHTML = texte;
 }
 
-
-// tableau 2d avec 10 lignes et 10 colonnes
-console.log("jeux tabel");
-Tableau2D(10, 10, "emplacement");
-let coordonneeX = 5 ;
-let coordonneeY = 2 ;
-//...
-// monTableau[coordonneeX][coordonneeY] = "Tresor" ;
-
-
+/*
 // choix() récupère l'ID de la case cliquée et traite le résultat
 function choix(that) {
 	console.log(that)
 document.getElementById("emplacement").innerHTML = enteteTable + corpsTable +finTable;
 
-}
-
-
-
+}*/
