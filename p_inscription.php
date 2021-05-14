436197b272ba66a971791497e069c923266a23f5
nathan varin <nathan.varin@outlook.fr>
2021-05-14 11:31:54 +0200

    modifications

--- p_inscription.php
+++ p_inscription.php
@@ -153,7 +153,6 @@ if($valid){
          <link href="<? = URL ?>css/jquery-ui.min.css" rel = "stylesheet" type = "text/css"/>
          <link href="<? = URL ?>css/style.css" rel ="stylesheet" type="text/css"/>
          <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-         
 </head>
 
 <body>
